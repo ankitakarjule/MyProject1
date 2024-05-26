5ebd5b909f9f50fcbbbb6aecd86c6e3633898e55
Ankita Karjule <ankitapadole111@gmail.com>
2024-05-26 17:27:21 +0530

    add some code

--- src/com/ankita/Demo2.java
+++ src/com/ankita/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("hello all");
 		System.out.println("welcome to git");
+		System.out.println("welcome to github");
 
 	}
 
